7f9320eadbf280a99f945a523034206786c2aff1
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-06 01:36:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObject = Object.assign({}, object)
-  delete newObject.key
+  var newObject = object
+  delete newObject.[key]
   console.log(newObject)
+  return newObject
 }
 
 deleteFromObjectByKey(recipes, 'oil')
